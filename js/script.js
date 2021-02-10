801e5bf6ed4d5eccc7d99bcd7d4063b1b06e3b7f
enstrong <75175712+enstrong@users.noreply.github.com>
2021-02-10 09:32:18 +0600

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,28 +1,111 @@
-let presidents = [
-	{ name: 'Касым-Жомарт', surname: 'Токаев', image: 'https://upload.wikimedia.org/wikipedia/commons/d/d3/Flag_of_Kazakhstan.svg', age: 67, country: 'Казахстана'},
-	{ name: 'Владимир', surname: 'Путин', image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f3/Flag_of_Russia.svg/250px-Flag_of_Russia.svg.png', age: 68, country: 'Росии'},
-	{ name: 'Джо', surname: 'Байден', image: 'https://upload.wikimedia.org/wikipedia/commons/e/e2/Flag_of_the_United_States_%28Pantone%29.svg', age: 78, country: 'США'},
-	{ name: 'Эмманюэль', surname: 'Макрон', image: 'https://upload.wikimedia.org/wikipedia/commons/c/c3/Flag_of_France.svg', age: 43, country: 'Франции'},
-	{ name: 'Франк-Вальтер', surname: 'Штайнмайер', image: 'https://upload.wikimedia.org/wikipedia/commons/b/ba/Flag_of_Germany.svg', age: 65, country: 'Германии'},
-];
+// более подробно https://www.youtube.com/watch?v=4QHFhIjF2L0
 
-function getRandomElement(arr) {
-	let randIndex = Math.floor(Math.random() * arr.length);
-	return arr[randIndex];
+
+// ПЕРЕМЕННЫЕ
+
+let message; // допустимые символы в названии переменной a-z, A-Z, 0-9, $, __
+let myMessage = "Сообщение";
+
+// ТИПЫ ДАННЫХ В ПЕРЕМЕННЫХ
+
+let myNumber = 10.983478932; // тип number
+let myString = "привет человечешка"; // тип String(строка)
+let myBoolean = true; // or false
+let myNull = null; // тип null являеться ничем, тоесть равно ничему(пустоте)
+let myUndefined = undefined; // тип undefined являеться впринципе ничем, тоесть просто 0. переменным которые не используют вообще присваивают это значение.
+// типы null и undefined не имеют других значении кроме null и undefined
+// let myObject = {
+//    name: "nursultan",
+//    age: 80,
+// };  тип обьект, составной тип который внутри себя может содержать различные свойства, среди которых могут быть и все выше перечисленные.
+
+
+// ЧИСЛА
+
+let number = 40 + 10;
+console.log(100 + number);
+console.log(100 - number);
+console.log(100 * number);
+console.log(100 / number);
+// меняем значение переменной
+number = number + 10; // длинная запись
+number += 10; // короткая запись
+number++; // если увеличивать на 1
+number--; // если уменьшать на 1
+console.log(number);
+console.log(Math.floor(10.5555)); // округление чисел до прошлой цифры
+console.log(Math.round(10.5555)); // округление чисел до ближайшей(обычное округление)
+console.log(Math.ceil(10.5555)); // округление чисел до следующей цифры
+
+let newNumber = 2.437;
+console.log(newNumber.toFixed(1));
+
+
+// СТРОКИ
+
+console.log("100" + number);
+console.log(myString + " как дела");
+console.log(myString.toUpperCase());
+
+
+// МАССИВЫ
+
+let names = ["Вася", "Саня", "Саша", "Петя", "Женя"];
+console.log(names[3]);
+
+names.push("Маша");
+console.log(names[5]); 
+
+
+// УСЛОВИЯ
+
+if ("Вася" != "Петя" && number > 80) {
+	console.log("true");
+} else {
+	console.log("false");
 }
 
-let name = document.querySelector('.president-name');
-let surname = document.querySelector('.president-surname');
-let age = document.querySelector('.president-age-area');
-let country = document.querySelector('.president-of-country');
-let image = document.querySelector('.country-img')
-let button = document.querySelector('.button');
 
-button.addEventListener('click', function () {
-	let randomElement = getRandomElement(presidents);
-	smoothly(name, 'textContent', randomElement.name);
-	smoothly(surname, 'textContent', randomElement.surname);
-	smoothly(age, 'textContent', randomElement.age);
-	smoothly(country, 'textContent', randomElement.country);
-	smoothly(image, 'src', randomElement.image);
-});
+// ЦИКЛЫ
+
+for (let i = 0; i < 10; i++) {
+	if (i == 6) {
+		break;
+	}
+
+	console.log(i);
+}
+
+for (let j = 0; j < names.length; j++) {
+	console.log(names[j]);
+}
+
+let i = 0;
+while (i < 10) {
+	console.log(i);
+	i++;
+}
+
+
+// ФУНКЦИИ
+
+function sum(x, y) {
+	return x + y;
+}
+
+console.log(sum(10, 59));
+
+
+// ОБЬЕКТЫ
+
+let myObject = {
+   name: "Nursultan",
+   surname: "Nazarbaev",
+   age: 80,
+   getFullName: function() {
+   	return this.name + " " + this.surname;
+   }		
+};
+
+console.log(myObject.name);
+console.log(myObject.getFullName());
